5c55e6440cec9fe81b1e77497a46c4a3c53b8dea
Kirill Korsakov <sadsanta@gmail.com>
2016-01-23 19:32:06 +0700

    carthage

---
+++ StrangeProject/StrangeService.swift
@@ -0,0 +1,15 @@
+//
+//  StrangeService.swift
+//  StrangeProject
+//
+//  Created by Kirill Korsakov on 23.01.16.
+//  Copyright © 2016 Kirill Korsakov. All rights reserved.
+//
+
+import Foundation
+
+class StrangeService {
+    func sum(a a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
---
+++ StrangeProjectTests/StrangeServiceSpec.swift
@@ -0,0 +1,24 @@
+//
+//  StrangeProjectTests.swift
+//  StrangeProjectTests
+//
+//  Created by Kirill Korsakov on 23.01.16.
+//  Copyright © 2016 Kirill Korsakov. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import StrangeProject
+
+class StrangeServiceSpec: QuickSpec {
+    override func spec() {
+        describe("StrangeService") {
+            it("sums two numbers") {
+                let service = StrangeService()
+                let sum = service.sum(a: 3, b: 2)
+                expect(sum).to(equal(5))
+            }
+        }
+    }
+    
+}
